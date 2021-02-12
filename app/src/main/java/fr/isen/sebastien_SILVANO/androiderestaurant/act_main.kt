6d68be7258a5a9930675439a60aea0acefd516b0
iasebsil83 <i.a.sebsil83@gmail.com>
2021-02-12 15:42:18 +0100

    added 1st design for act_main

--- app/src/main/java/fr/isen/sebastien_SILVANO/androiderestaurant/act_main.kt
+++ app/src/main/java/fr/isen/sebastien_SILVANO/androiderestaurant/act_main.kt
@@ -3,8 +3,17 @@ package fr.isen.sebastien_SILVANO.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class act_main : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+
+
+class act_main : AppCompatActivity(){
+
+
+
+    //init
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.lyo_main)
     }
