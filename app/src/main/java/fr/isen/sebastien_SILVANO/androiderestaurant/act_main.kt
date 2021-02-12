6db156160b52ec4230323dd905ec15fa653f5cc4
iasebsil83 <i.a.sebsil83@gmail.com>
2021-02-12 13:24:37 +0100

    Initial commit

---
+++ app/src/main/java/fr/isen/sebastien_SILVANO/androiderestaurant/act_main.kt
@@ -0,0 +1,11 @@
+package fr.isen.sebastien_SILVANO.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class act_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lyo_main)
+    }
+}
