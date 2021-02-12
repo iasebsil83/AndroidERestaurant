2cf967dc4c6478ee46eeedca63ded397c679744e
iasebsil83 <i.a.sebsil83@gmail.com>
2021-02-12 17:41:09 +0100

    finished first activity

--- app/src/main/java/fr/isen/sebastien_SILVANO/androiderestaurant/act_main.kt
+++ app/src/main/java/fr/isen/sebastien_SILVANO/androiderestaurant/act_main.kt
@@ -2,9 +2,9 @@ package fr.isen.sebastien_SILVANO.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-
-
+import android.widget.Button
+import android.widget.Toast
+import fr.isen.sebastien_SILVANO.androiderestaurant.databinding.LyoMainBinding
 
 
 
@@ -12,9 +12,23 @@ class act_main : AppCompatActivity(){
 
 
 
+    //binding
+    private lateinit var binding : LyoMainBinding
+
+
+
     //init
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.lyo_main)
+
+        binding = LyoMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mainAction = findViewById<Button>(R.id.entrees)
+
+        binding.entrees.setOnClickListener{
+            val toast = Toast.makeText(applicationContext, "entree", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
